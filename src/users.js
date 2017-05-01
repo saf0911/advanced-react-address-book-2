a27fc8b78343cdeef0212a6ba6e789622167135f
Ivo Meißner <info@overtronic.com>
2017-05-01 17:27:59 -0500

    Added state values for further exercises

--- src/users.js
+++ src/users.js
@@ -1,44 +1,48 @@
 export default [
   {
     "id": 1,
-    "first_name": "george",
-    "last_name": "bluth",
+    "firstName": "george",
+    "lastName": "bluth",
     "address": "4116 Magnolia Drive, Portland, ME 04103",
     "phone": 15551234567,
     "occupation": "father",
+    "state": "TX",
     "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
   },
   {
     "id": 2,
-    "first_name": "lucille",
-    "last_name": "bluth",
+    "firstName": "lucille",
+    "lastName": "bluth",
     "address": "6428 3rd Street East, Zion, IL 60099",
     "phone": 15552345678,
     "occupation": "mother",
+    "state": "TX",
     "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
   },
   {
     "id": 3,
-    "first_name": "oscar",
-    "last_name": "bluth",
+    "firstName": "oscar",
+    "lastName": "bluth",
     "address": "4797 Sycamore Lane, Dover, NH 03820",
     "phone": 15553456789,
     "occupation": "uncle",
+    "state": "TX",
     "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
   },
   {
     "id": 4,
-    "first_name": "steve",
-    "last_name": "holt",
+    "firstName": "steve",
+    "lastName": "holt",
     "address": "3722 Circle Drive, Bristow, VA 20136",
     "phone": 15554567890,
     "occupation": "friend",
+    "state": "AZ",
     "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg"
   },
   {
     "id": 5,
-    "first_name": "gob",
-    "last_name": "bluth",
+    "firstName": "gob",
+    "lastName": "bluth",
     "address": "959 Hilltop Road, Apple Valley, CA 92307",
     "phone": 15555678901,
     "occupation": "brother",
@@ -46,15 +50,13 @@ export default [
   },
   {
     "id": 6,
-    "first_name": "tracey",
-    "last_name": "bluth",
+    "firstName": "tracey",
+    "lastName": "bluth",
     "address": "2640 Primrose Lane, Longview, TX 75604",
     "phone": 15556789012,
     "occupation": "wife",
+    "state": "AR",
     "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg"
   }
 
 ];
-
-
-
